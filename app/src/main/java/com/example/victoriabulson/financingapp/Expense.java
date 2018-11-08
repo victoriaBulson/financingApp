f3ed26562513f0698373d07a1b9e536ffc89dbbe
victoriabulson <victoriabulson@gmail.com>
2018-11-08 11:47:33 -0700

    Merge branch 'master' of https://github.com/victoriaBulson/financingApp
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    
    Finished Expense unit tests and stub

--- app/src/main/java/com/example/victoriabulson/financingapp/Expense.java
+++ app/src/main/java/com/example/victoriabulson/financingapp/Expense.java
@@ -1,11 +1,41 @@
 package com.example.victoriabulson.financingapp;
 
 class Expense {
-    String categoryName;
-    public void setCategoryName(String rent) {
+
+    private String categoryName;
+    private double budget = 0;
+    private double spent = 0;
+    private boolean fixed = false;
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
     }
 
     public String getCategoryName() {
         return categoryName;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setSpent(double spent) {
+        this.spent = spent;
+    }
+
+    public double getSpent() {
+        return spent;
+    }
+
+    public void setFixed(boolean fixed) {
+        this.fixed = fixed;
+    }
+
+    public boolean getFixed() {
+        return fixed;
     }
 }
--- app/src/test/java/com/example/victoriabulson/financingapp/ExpenseTest.java
+++ app/src/test/java/com/example/victoriabulson/financingapp/ExpenseTest.java
@@ -11,13 +11,38 @@ import static org.junit.Assert.*;
  */
 public class ExpenseTest {
     @Test
-    public void test_expense(){
+    public void test_category_name(){
         Expense rentObject = new Expense();
         //TEST CATEGORY NAME
         rentObject.setCategoryName("rent");
         assert("rent" == rentObject.getCategoryName());
-
     }
+
+    @Test
+    public void test_budget(){
+        Expense rentObject = new Expense();
+        //TEST CATEGORY NAME
+        rentObject.setBudget(1000);
+        assert(1000 == rentObject.getBudget());
+    }
+
+    @Test
+    public void test_spent(){
+        Expense rentObject = new Expense();
+        //TEST CATEGORY NAME
+        rentObject.setSpent(500);
+        assert(500 == rentObject.getSpent());
+    }
+
+    @Test
+    public void test_fixed(){
+        Expense rentObject = new Expense();
+        //TEST CATEGORY NAME
+        rentObject.setFixed(true);
+        assert(true == rentObject.getFixed());
+    }
+
+
 
 
 }

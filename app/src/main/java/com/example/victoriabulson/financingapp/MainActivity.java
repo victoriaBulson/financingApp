4db8dec4e7561e7ea6ffc31820ead957529c387e
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2018-11-08 12:52:38 -0700

    Able to open Budgetplanning activity

--- app/src/main/java/com/example/victoriabulson/financingapp/MainActivity.java
+++ app/src/main/java/com/example/victoriabulson/financingapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.victoriabulson.financingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view){
+        Intent mintent = new Intent(MainActivity.this, BudgettingPlanning.class);
+        startActivity(mintent);
     }
 }

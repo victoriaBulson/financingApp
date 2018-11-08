02d3df21d436c8d23e7661b2d22fc300162925f1
victoriabulson <victoriabulson@gmail.com>
2018-11-08 12:56:59 -0700

    Merge branch 'master' of https://github.com/victoriaBulson/financingApp
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    
    Tried to open budget Planning activity -- failed

--- app/src/main/java/com/example/victoriabulson/financingapp/MainActivity.java
+++ app/src/main/java/com/example/victoriabulson/financingapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.victoriabulson.financingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button NewActivityBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NewActivityBtn = (Button) findViewById(R.id.planBugetButton);
+
+         NewActivityBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BudgetPlanningActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }

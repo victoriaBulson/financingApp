83b8602a159a280afa8a2dd66e9b3596067ee739
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2018-11-08 13:10:48 -0700

    do not push

--- app/src/main/java/com/example/victoriabulson/financingapp/BudgetPlanning.java
+++ app/src/main/java/com/example/victoriabulson/financingapp/BudgetPlanning.java
@@ -3,7 +3,7 @@ package com.example.victoriabulson.financingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BudgettingPlanning extends AppCompatActivity {
+public class BudgetPlanning extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

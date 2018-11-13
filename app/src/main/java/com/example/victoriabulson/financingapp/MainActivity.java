5963bd79ae0784f2bd965d76ad18266aeaeb35fc
victoriabulson <victoriabulson@gmail.com>
2018-11-13 11:57:52 -0700

    Finished creating initial Expense List in Main Activity

--- app/src/main/java/com/example/victoriabulson/financingapp/MainActivity.java
+++ app/src/main/java/com/example/victoriabulson/financingapp/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,7 +18,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonClick(View view){
-        Intent mintent = new Intent(MainActivity.this, BudgetPlanning.class);
-        startActivity(mintent);
+        Intent plannerIntent = new Intent(MainActivity.this, BudgetPlanning.class);
+
+        List<Expense> expenseList = new ArrayList<>(11);
+        Expense rentObject = new Expense(getString(R.string.Rent));
+        expenseList.add(rentObject);
+        Expense transportObject = new Expense(getString(R.string.Transportation));
+        expenseList.add(transportObject);
+        Expense foodObject = new Expense(getString(R.string.Food));
+        expenseList.add(foodObject);
+        Expense utilitiesObject = new Expense(getString(R.string.Utilities));
+        expenseList.add(utilitiesObject);
+        Expense entertainmentObject = new Expense(getString(R.string.Entertainment));
+        expenseList.add(entertainmentObject);
+        Expense savingsObject = new Expense(getString(R.string.Savings));
+        expenseList.add(savingsObject);
+        Expense emergencyFundObject = new Expense(getString(R.string.EmergencyFund));
+        expenseList.add(emergencyFundObject);
+        Expense personalCareObject = new Expense(getString(R.string.PersonalCare));
+        expenseList.add(personalCareObject);
+        Expense healthCareObject = new Expense(getString(R.string.HealthCare));
+        expenseList.add(healthCareObject);
+        Expense debtObject = new Expense(getString(R.string.Debt));
+        expenseList.add(debtObject);
+        Expense otherObject = new Expense(getString(R.string.Other));
+        expenseList.add(otherObject);
+
+
+        //plannerIntent.putExtra(EXPENSE_ARRAY, expenseList);
+        startActivity(plannerIntent);
     }
 }

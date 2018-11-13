1841d211097c63593672cfefaf17f0bd588a3fc0
Bradleehope <43479958+Bradleehope@users.noreply.github.com>
2018-11-13 14:18:05 -0700

    added budgetPlanning active checkboxes and submit button

--- app/src/main/java/com/example/victoriabulson/financingapp/BudgetPlanning.java
+++ app/src/main/java/com/example/victoriabulson/financingapp/BudgetPlanning.java
@@ -1,7 +1,9 @@
 package com.example.victoriabulson.financingapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class BudgetPlanning extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class BudgetPlanning extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_planning);
+        }
+    public void buttonClick(View view){
+        Intent plannerIntent = new Intent(BudgetPlanning.this, MainActivity.class);
     }
 }

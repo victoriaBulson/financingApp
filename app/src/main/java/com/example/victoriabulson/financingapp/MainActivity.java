60ac6064b4705054ba3d21673a2ed5349061f91d
victoriabulson <victoriabulson@gmail.com>
2018-11-15 11:39:28 -0700

    Debugging output for sharedPreferences

--- app/src/main/java/com/example/victoriabulson/financingapp/MainActivity.java
+++ app/src/main/java/com/example/victoriabulson/financingapp/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.victoriabulson.financingapp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.nfc.Tag;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 
@@ -31,31 +33,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Gson gson = new Gson();
+        savedBudget = getPreferences(Context.MODE_PRIVATE);
+        String json = savedBudget.getString("savedBudgetKey", null);
+            //PRINTS PULLED STRING
+            final String TAG = "MAIN_ACTIVITY";
+            Log.d(TAG, "JSON IS: ");
+            Log.d(TAG, json);                   //Json always prints as null (11/14)
 
-        Expense rentObject = new Expense(getString(R.string.Rent));
-        expenseList.add(rentObject);
-        Expense transportObject = new Expense(getString(R.string.Transportation));
-        expenseList.add(transportObject);
-        Expense foodObject = new Expense(getString(R.string.Food));
-        expenseList.add(foodObject);
-        Expense utilitiesObject = new Expense(getString(R.string.Utilities));
-        expenseList.add(utilitiesObject);
-        Expense entertainmentObject = new Expense(getString(R.string.Entertainment));
-        expenseList.add(entertainmentObject);
-        Expense savingsObject = new Expense(getString(R.string.Savings));
-        expenseList.add(savingsObject);
-        Expense emergencyFundObject = new Expense(getString(R.string.EmergencyFund));
-        expenseList.add(emergencyFundObject);
-        Expense personalCareObject = new Expense(getString(R.string.PersonalCare));
-        expenseList.add(personalCareObject);
-        Expense healthCareObject = new Expense(getString(R.string.HealthCare));
-        expenseList.add(healthCareObject);
-        Expense debtObject = new Expense(getString(R.string.Debt));
-        expenseList.add(debtObject);
-        Expense otherObject = new Expense(getString(R.string.Other));
-        expenseList.add(otherObject);
+        if (json != null) {                                                 //json never evaluates as null (11/14)
+            Log.d(TAG, "IF");
+            Expense rentObject = new Expense(getString(R.string.Rent));
+            expenseList.add(rentObject);
+            Expense transportObject = new Expense(getString(R.string.Transportation));
+            expenseList.add(transportObject);
+            Expense foodObject = new Expense(getString(R.string.Food));
+            expenseList.add(foodObject);
+            Expense utilitiesObject = new Expense(getString(R.string.Utilities));
+            expenseList.add(utilitiesObject);
+            Expense entertainmentObject = new Expense(getString(R.string.Entertainment));
+            expenseList.add(entertainmentObject);
+            Expense savingsObject = new Expense(getString(R.string.Savings));
+            expenseList.add(savingsObject);
+            Expense emergencyFundObject = new Expense(getString(R.string.EmergencyFund));
+            expenseList.add(emergencyFundObject);
+            Expense personalCareObject = new Expense(getString(R.string.PersonalCare));
+            expenseList.add(personalCareObject);
+            Expense healthCareObject = new Expense(getString(R.string.HealthCare));
+            expenseList.add(healthCareObject);
+            Expense debtObject = new Expense(getString(R.string.Debt));
+            expenseList.add(debtObject);
+            Expense otherObject = new Expense(getString(R.string.Other));
+            expenseList.add(otherObject);
 
-        savedBudget = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+            savedBudget = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        }
+        else {
+            expenseList = gson.fromJson(json, List.class);
+            Log.d(TAG, "ELSE");
+        }
+
 
     }
 
@@ -65,9 +82,16 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences.Editor preferencesEditor = savedBudget.edit();
         Gson gson = new Gson();
-        String thingamabob = gson.toJson(expenseList);
-        preferencesEditor.putString("Da List", thingamabob);
+        String expenseListString = gson.toJson(expenseList);
+        preferencesEditor.putString("savedBudgetKey", expenseListString);
         preferencesEditor.commit();
+        final String TAG = "MAIN_ACTIVITY";
+            //PRINTS SAVED STRING
+            String json = savedBudget.getString("savedBudgetKey", null);
+            Log.d(TAG, "THIS WAS SAVED: ");
+            Log.d(TAG, json);
+
+
     }
 
     public void buttonClick(View view){
